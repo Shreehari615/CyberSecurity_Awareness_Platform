ac5f031f216203d8dd61ca307653df8eaa7b06e2
Shreehari615 <shreeharisvbhat2005@gmail.com>
2026-07-09 13:27:08 +0530

    done

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -194,7 +194,7 @@ print("EMAIL_PORT repr:", repr(os.getenv("EMAIL_PORT")))'''
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = "587"
+EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
 EMAIL_HOST_USER = "cybersecurityapplication@gmail.com"
